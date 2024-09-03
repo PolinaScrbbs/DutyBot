0fa0a19806da2e6c93a4c015d24fb1698361413c
PolinaScrbbs <polinascaraboobs@gmail.com>
2024-09-03 11:50:02 +0300

    Update Group FSMContext
    -Fix Student in Group Students List
    -Fix Group Kick

--- bot/app/handlers/group.py
+++ bot/app/handlers/group.py
@@ -9,9 +9,6 @@ from database import get_async_session
 from .ungroup import router
 
 
-#Меню группы============================================================================================================
-
-
 @router.message(lambda message: message.text == "Группа")
 async def group_menu(message: Message, state: FSMContext):
     session = await get_async_session()
@@ -19,9 +16,11 @@ async def group_menu(message: Message, state: FSMContext):
     group = await rq.get_group_by_id_with_students_and_applications(session, user.group_id, user.id)
     await session.close()
 
-    await state.update_data(data={
-        "user": user,
-        "group": group
+    await state.update_data({
+        message.from_user.id: {
+            "user": user,
+            "group": group
+        }
     })
 
     applications_count = len(group.applications)
@@ -29,31 +28,31 @@ async def group_menu(message: Message, state: FSMContext):
     if applications_count != 0:
         applications_count = f'({applications_count})'
     else:
-        applications_count = None
+        applications_count = 0
     
     await message.answer(f"*{group.title.upper()}*", parse_mode="Markdown", reply_markup=await kb.group_menu(applications_count))
     
     
 @router.callback_query(F.data == 'students')
-async def students(callback:CallbackQuery, state: FSMContext):
+async def students(callback: CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    group = data['group']
+    group = data[callback.from_user.id]['group']
 
-    if group.students == []:
+    if not group.students:
         await callback.message.edit_text('Список студентов пуст')
-
     else:
         await callback.message.edit_text('*Студенты*', parse_mode="Markdown", reply_markup=await kb.inline_students(group.students))
 
+
 @router.callback_query(lambda query: query.data.startswith('st_'))
-async def student(callback: CallbackQuery, state: FSMContext):
+async def student(callback: CallbackQuery):
     await callback.message.delete_reply_markup()
     student_fields = callback.data.split('_')
-    student_username = student_fields[2]
+    student_id = int(student_fields[1])
 
     try:
         session = await get_async_session()
-        student = await rq.get_user_by_username(session, student_username)
+        student = await rq.get_user_by_id(session, student_id)
 
         await callback.message.edit_text(
             f'*@{student.username}*\n{student.surname} {student.name}', 
@@ -64,8 +63,9 @@ async def student(callback: CallbackQuery, state: FSMContext):
     finally:
         await session.close()
 
+
 @router.callback_query(lambda query: query.data.startswith('delete_student_'))
-async def student_kick(callback: CallbackQuery, state: FSMContext):
+async def student_kick(callback: CallbackQuery):
     session = await get_async_session()
     student_fields = callback.data.split('_')
     student_id = int(student_fields[2])
@@ -77,23 +77,23 @@ async def student_kick(callback: CallbackQuery, state: FSMContext):
         parse_mode='Markdown'
     )
 
-    # await callback.message.answer(f"*{group_title.upper()}*", reply_markup=kb.group_menu, parse_mode="Markdown")
 
 @router.callback_query(F.data == 'grp_applications')
 async def group_applications_list(callback: CallbackQuery, state: FSMContext):
     session = await get_async_session()
     data = await state.get_data()
-    group = data['group']
+    group = data[callback.from_user.id]['group']
 
     applications = await rq.get_group_applications(session, group.id)
 
-    if applications == []:
+    if not applications:
         await callback.message.edit_text('Список заявок пуст')
     else:
         await callback.message.edit_text('*Заявки*', parse_mode='Markdown', reply_markup=await kb.inline_group_applications(applications))
 
+
 @router.callback_query(lambda query: query.data.startswith('grp_application_'))
-async def group_application(callback: CallbackQuery, state: FSMContext):
+async def group_application(callback: CallbackQuery):
     session = await get_async_session()
 
     callback_data_fields = callback.data.split('_')
@@ -105,8 +105,11 @@ async def group_application(callback: CallbackQuery, state: FSMContext):
 
     await callback.message.edit_text(f'Заявка @*{user.username}* ({user.surname} {user.name})', parse_mode='Markdown', reply_markup=await kb.inline_application(application))
 
+    await session.close()
+
+
 @router.callback_query(lambda query: query.data.startswith('accept_application_'))
-async def accept_group_application(callback: CallbackQuery, state: FSMContext):
+async def accept_group_application(callback: CallbackQuery):
     session = await get_async_session()
 
     callback_data_fields = callback.data.split('_')
@@ -119,20 +122,21 @@ async def accept_group_application(callback: CallbackQuery, state: FSMContext):
 
     await callback.message.edit_text(f'{msg}', parse_mode='Markdown')
 
-    if applications == []:
+    if not applications:
         await callback.message.edit_text('Список заявок пуст')
     else:
         await callback.message.edit_text('*Заявки*', parse_mode='Markdown', reply_markup=await kb.inline_group_applications(applications))
 
+    await session.close()
+
+
 @router.callback_query(lambda query: query.data.startswith('rejected_application_'))
-async def rejected_group_application(callback: CallbackQuery, state: FSMContext):
+async def rejected_group_application(callback: CallbackQuery):
     session = await get_async_session()
 
     callback_data_fields = callback.data.split('_')
     application_id = int(callback_data_fields[2])
     sending_id = int(callback_data_fields[3])
-
-    print(application_id, sending_id)
 
     application, msg = await rq.rejected_application(session, application_id, sending_id)
 
@@ -140,9 +144,11 @@ async def rejected_group_application(callback: CallbackQuery, state: FSMContext)
 
     await callback.message.edit_text(f'{msg}', parse_mode='Markdown')
 
-    if applications == []:
+    if not applications:
         await callback.message.edit_text('Список заявок пуст')
     else:
         await callback.message.edit_text('*Заявки*', parse_mode='Markdown', reply_markup=await kb.inline_group_applications(applications))
+
+    await session.close()
 
     
