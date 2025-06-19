abe4292acc207f97ad4d85f31c2938a0853cf72c
PolinaScrbbs <147543684+PolinaScrbbs@users.noreply.github.com>
2025-06-19 15:57:02 +0300

    Update database.py

--- api/database.py
+++ api/database.py
@@ -1,35 +1,22 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
 from .config import config as conf
-# Создаем синхронный engine с увеличенными таймаутами
-engine = create_engine(
-    conf.database_url,
-    echo=True,
-    # pool_pre_ping=True,
-    # pool_size=20,
-    # max_overflow=10,
-    # pool_timeout=30,  # 30 секунд ожидания соединения из пула
-    # pool_recycle=3600
-)
 
-# Создаем фабрику сессий
-SessionLocal = sessionmaker(
+engine = create_async_engine(conf.database_url, echo=True)
+
+async_session = async_sessionmaker(
     bind=engine,
-    autocommit=False,
-    autoflush=False,
-    expire_on_commit=False
+    class_=AsyncSession,
+    expire_on_commit=False,
 )
 
-def get_session():
-    """Генератор сессий (синхронный)"""
-    session = SessionLocal()
-    try:
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
         yield session
-    except Exception:
-        session.rollback()
-        raise
-    finally:
-        session.close()
+
 
 class Base(DeclarativeBase):
     pass
