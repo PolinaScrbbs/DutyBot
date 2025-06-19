547a9191b666f03006097ce747c5b2427879cba8
PolinaScrbbs <polinascaraboobs@gmail.com>
2025-06-19 09:06:46 +0300

    DB Connection Fix

--- api/database.py
+++ api/database.py
@@ -1,29 +1,37 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-
 from .config import config as conf
+
 
 engine = create_async_engine(
     conf.database_url,
     echo=True,
+    pool_pre_ping=True,
     connect_args={
         "statement_cache_size": 0,
-        "prepared_statement_cache_size": 0
+        "prepared_statement_cache_size": 0,
+        "server_settings": {
+            "jit": "off"
+        }
     }
 )
+
 
 async_session = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False,
+    autoflush=False,
+    future=True
 )
-
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
-        yield session
-
+        try:
+            yield session
+        finally:
+            await session.close()
 
 class Base(DeclarativeBase):
     pass
