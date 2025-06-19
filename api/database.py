8d78375c97db6e2fdaf529577239b71ea122b279
PolinaScrbbs <147543684+PolinaScrbbs@users.noreply.github.com>
2025-06-19 15:31:31 +0300

    Update database.py

--- api/database.py
+++ api/database.py
@@ -5,18 +5,11 @@ from .config import config as conf
 engine = create_engine(
     conf.database_url,
     echo=True,
-    pool_pre_ping=True,
-    pool_size=20,
-    max_overflow=10,
-    pool_timeout=30,  # 30 секунд ожидания соединения из пула
-    pool_recycle=3600,
-    connect_args={
-        "connect_timeout": 30,  # 30 секунд на установку соединения
-        "keepalives": 1,
-        "keepalives_idle": 30,
-        "keepalives_interval": 10,
-        "keepalives_count": 5
-    }
+    # pool_pre_ping=True,
+    # pool_size=20,
+    # max_overflow=10,
+    # pool_timeout=30,  # 30 секунд ожидания соединения из пула
+    # pool_recycle=3600
 )
 
 # Создаем фабрику сессий
